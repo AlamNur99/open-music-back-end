d4737dddbccbea62394431a456339257461fa6ea
Muhammad Alam Nur Haris <muh.alamnur.haris99@gmail.com>
2023-04-17 19:39:04 +0800

    add auto bind at handler

--- src/api/song/handler.js
+++ src/api/song/handler.js
@@ -1,15 +1,11 @@
-const ClientError = require("../../exceptions/ClientError");
+const autoBind = require('auto-bind');
 
 class SongHandler {
     constructor(service, validator) {
         this._service = service;
         this._validator = validator;
 
-        this.postSongHandler = this.postSongHandler.bind(this);
-        this.getSongshandler = this.getSongshandler.bind(this);
-        this.getSongByIdHandler = this.getSongByIdHandler.bind(this);
-        this.putSongByIdHandler = this.putSongByIdHandler.bind(this);
-        this.deleteSongByIdHandler = this.deleteSongByIdHandler.bind(this);
+        autoBind(this);
     }
 
     async postSongHandler(request, h) {
